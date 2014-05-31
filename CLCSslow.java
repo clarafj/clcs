48ed915d7893e19121b3436fecc8e00de91bec5f
John Louie <jlouie95618@gmail.com>
2014-05-31 16:13:36 -0700

    "Second Commit -John, added all files to commit, part (f) working!"

--- CLCSslow.java
+++ CLCSslow.java
@@ -1,33 +1,51 @@
 import java.util.*;
 
+/*
+ *
+ *
+ *
+ */
 
 public class CLCSslow {
-  
+
   static int[][] arr = new int[2048][2048];
-  static char[] A, B;
+  static String A = "", B = "";
 
   static int CLCS() {
-    int m = A.length, n = B.length;
-    int i, j;
-    for (i = 0; i <= m; i++) arr[i][0] = 0;
-    for (j = 0; j <= n; j++) arr[0][j] = 0;
-    
-    for (i = 1; i <= m; i++) {
-      for (j = 1; j <= n; j++) {
-        arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
-        if (A[i-1] == B[j-1]) arr[i][j] = Math.max(arr[i][j], arr[i-1][j-1]+1);
+
+    int m = A.length(), n = B.length();
+
+    ArrayList<Integer> values = new ArrayList<Integer>();
+
+    for (int k = 0; k < m; k++) {
+
+      String Aprime = A.substring(k) + A.substring(0, k);
+
+      int i, j;
+      for (i = 0; i <= m; i++) arr[i][0] = 0;
+      for (j = 0; j <= n; j++) arr[0][j] = 0;
+      
+      for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) {
+          arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+          if (Aprime.charAt(i-1) == B.charAt(j-1)) arr[i][j] = Math.max(arr[i][j], arr[i-1][j-1]+1);
+        }
       }
-    }
-    
-    return arr[m][n];
+
+      values.add(arr[m][n]);
+
+    } 
+
+    return Collections.max(values);
+
   }
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     int T = s.nextInt();
     for (int tc = 0; tc < T; tc++) {
-      A = s.next().toCharArray();
-      B = s.next().toCharArray();
+      A = s.next();
+      B = s.next();
       System.out.println(CLCS());
     }
   }
